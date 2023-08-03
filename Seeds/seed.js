a4b319ecd8282c80c982e6dffbfc2f0d18f8f256
mayaj0yce <m.j.thayne@gmail.com>
2023-08-03 13:15:29 -0600

    table save point.

--- Seeds/seed.js
+++ Seeds/seed.js
@@ -1,26 +1,26 @@
 const sequelize = require('../config/connection');
 const { Artist, Art } = require('../models');
-
+// calls model data
 const artistSeedData = require('./artistSeedData.json');
 const artSeedData = require('./artSeedData.json');
-
+// 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const artists = await Artist.bulkCreate(artistSeedData);
+  const art = await Artist.bulkCreate(artistSeedData);
 
-  for (const { id } of artists) {
+  for (const { artist } of art) {
     const newArt = await Art.create({
-      artist_id: id,
+      art_id: id,
     });
   }
 
-  for (const art of artSeedData) {
-    const newArt = await Art.create({
-      ...art,
-      artist_id: artists[Math.floor(Math.random() * artists.length)].id,
-    });
-  }
+//   for (const art of artSeedData) {
+//     const newArt = await Art.create({
+//       ...art,
+//       artist_id: artists[Math.floor(Math.random() * artists.length)].id,
+//     });
+//   }
 
   process.exit(0);
 };
---
+++ models/Artist.js
@@ -0,0 +1,31 @@
+const {Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Artist extends Model {}
+
+Artist.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        firstArt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true,
+            },
+          },
+    }
+)
